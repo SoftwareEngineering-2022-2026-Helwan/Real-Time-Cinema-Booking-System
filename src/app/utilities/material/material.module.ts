86aea5d484fc1479702d6eb8f110dda992ed822c
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2024-12-03 01:20:43 +0200

    main packages installed

---
+++ src/app/utilities/material/material.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list'
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import {MatDialogModule} from '@angular/material/dialog'
+const material = [
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatStepperModule,
+    MatInputModule,
+    MatCardModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatDialogModule
+]
+@NgModule({
+    imports: [material],
+    providers: [{
+        provide: STEPPER_GLOBAL_OPTIONS,
+        useValue: {
+            showError: true,
+            displayDefaultIndicatorType: false,
+        },
+      }],
+    exports: [material]
+})
+export class MaterialModule { }
