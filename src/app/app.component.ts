db91d5cc9dea662ffdef04417a9bb078a5310a9c
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2024-12-03 18:40:25 +0200

    card Acceptance Test Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  movies = [
+    {
+      h_src: '../assets/imgs/postersH/1.jpg',
+      name: 'The Dark Knight',
+      category: 'Action',
+      duration: '2h 32m',
+      cinema: 'Cinema 1'
+    },
+    {
+      h_src: '../assets/imgs/postersH/10.jpg',
+      name: 'The Lord of the Rings: The Return of the King',
+      category: 'Adventure',
+      duration: '3h 21m',
+      cinema: 'Cinema 2'
+    },
+    {
+      h_src: 'https://pbs.twimg.com/media/Ei9Oka8WAAQ7j4j?format=jpg&name=large',
+      name: 'Pulp Fiction',
+      category: 'Crime',
+      duration: '2h 34m',
+      cinema: 'Cinema 3'
+    }
+  ];
+
 
 }
+
--- src/app/components/movieCard/detailed-movieCard/detailed-movieCard.component.ts
+++ src/app/components/movieCard/detailed-movieCard/detailed-movieCard.component.ts
@@ -6,6 +6,20 @@ import { Movie } from '../../../interfaces/movie.interface';
   templateUrl: './detailed-movieCard.component.html',
   styleUrls: ['./detailed-movieCard.component.css']
 })
+
+/*
+
+this component input came from url and services 
+=========================
+to display the card in good shape apply this css
+.list
+{
+    margin: 5vh 0;
+    padding: 0 10vw;
+}
+
+*/ 
+
 export class DetailedMovieCardComponent implements OnInit {
 
   constructor() { }
