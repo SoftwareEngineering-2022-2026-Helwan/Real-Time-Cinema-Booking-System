a930ee5953bdb7680ebd9482f2b1d25b22bbc8e6
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2024-12-03 03:02:09 +0200

    error pages added !

---
+++ src/app/components/errors/NotAuthorized/NotAuthorized.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-NotAuthorized',
+  templateUrl: './NotAuthorized.component.html',
+  styleUrls: ['./NotAuthorized.component.css']
+})
+export class NotAuthorizedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
