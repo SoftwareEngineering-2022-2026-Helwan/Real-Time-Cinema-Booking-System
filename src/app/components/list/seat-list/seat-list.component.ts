8fcfeb85eae468476a5185fc2f5da7b6bf8b5015
Salah Elkhatib <salahelkhatib004@gmail.com>
2024-12-10 18:54:04 +0200

    Seating Map

--- src/app/components/list/seat-list/seat-list.component.ts
+++ src/app/components/list/seat-list/seat-list.component.ts
@@ -3,11 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-seat-list',
   templateUrl: './seat-list.component.html',
-  styleUrls: ['./seat-list.component.css']
+  styleUrls: ['./seat-list.component.css'],
 })
 export class SeatListComponent implements OnInit {
+  // Example arrays
+  option1Array: string[] = ['A2', 'D4', 'F3', 'C16'];
+  option2Array: string[] = ['D2', 'H12', 'H13', 'H15'];
+  option3Array: string[] = ['A14', 'B13', 'B15', 'B16'];
 
-  constructor() { }
+
+  selectedArray: string[] = [];
+
+  onOptionChange(selectedOption: string): void {
+    switch (selectedOption) {
+      case 'option1':
+        this.selectedArray = this.option1Array;
+        break;
+      case 'option2':
+        this.selectedArray = this.option2Array;
+        break;
+      case 'option3':
+        this.selectedArray = this.option3Array;
+        break;
+      default:
+        this.selectedArray = [];
+    }
+  }
+
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- src/app/components/seat/seat.component.ts
+++ src/app/components/seat/seat.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeatComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
