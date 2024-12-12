2147c8ef046cdefcabe958ff851c9b9633fd9074
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2024-12-13 00:32:24 +0200

    movie array added to fix view problem

--- src/app/pages/customer/reservation-page/reservation-page.component.ts
+++ src/app/pages/customer/reservation-page/reservation-page.component.ts
@@ -60,6 +60,29 @@ export class ReservationPageComponent implements OnInit {
     this.markers.content = parser.parseFromString(svgString, "image/svg+xml").documentElement;
   }
 
+  movies = [
+    {
+      h_src: '../assets/imgs/postersH/1.jpg',
+      name: 'The Dark Knight',
+      category: 'Action',
+      duration: '2h 32m',
+      cinema: 'Cinema 1'
+    },
+    {
+      h_src: '../assets/imgs/postersH/10.jpg',
+      name: 'The Lord of the Rings: The Return of the King',
+      category: 'Adventure',
+      duration: '3h 21m',
+      cinema: 'Cinema 2'
+    },
+    {
+      h_src: 'https://pbs.twimg.com/media/Ei9Oka8WAAQ7j4j?format=jpg&name=large',
+      name: 'Pulp Fiction',
+      category: 'Crime',
+      duration: '2h 34m',
+      cinema: 'Cinema 3'
+    }
+  ];
 
   zoom = 7;
   center: any = {
