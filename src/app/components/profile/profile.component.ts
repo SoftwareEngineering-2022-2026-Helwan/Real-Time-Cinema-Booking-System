e66a0ed59dc954123bf9608066b89c828324c405
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2024-12-12 22:01:27 +0200

    profile fixed !!

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -7,41 +7,79 @@ import { FormGroup, FormControl, FormBuilder,ValidationErrors, Validators, Valid
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
- active: boolean = true;
- showPass: Boolean = false;
- myForm: FormGroup;
+    isViewMode: boolean;
+    showPass: Boolean = false;
+    
 
+    data = {
+        fname: 'John',
+        lname: 'Doe',
+        pass: '********',
+        phone: '20123456781',
+        email: 'test@gmail.com',
+    }
   constructor(private fb: FormBuilder) {
-    this.myForm = this.fb.group({
-      fname: new FormControl('John'),
-      lname: new FormControl('Smith'),
-      pass: new FormControl('123456'),
-      phone: new FormControl('+20************'),
-      email: new FormControl('test@gmail.com'),
-    });
+    this.isViewMode = true;
+    this.signupForm.disable();
   }
 
   
   signupForm : FormGroup = new FormGroup({
-    email : new FormControl(null,[Validators.required,Validators.email]),
-    fname : new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
-    lname : new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
-    password : new FormControl(null,[Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
+    email : new FormControl(this.data.email,[Validators.required,Validators.email]),
+    fname : new FormControl(this.data.fname,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+    lname : new FormControl(this.data.lname,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+    password : new FormControl(this.data.pass,[Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
     Validators.minLength(8),Validators.maxLength(15)]),
-    phone : new FormControl(null,[Validators.required,Validators.minLength(11), Validators.maxLength(13)]),
+    phone : new FormControl(this.data.phone,[Validators.required,Validators.minLength(11), Validators.maxLength(13)]),
   })
+  
+  resetForm() {
+    this.signupForm.reset(this.data);
+    this.displayLoader();
+    this.toggleEdit();
+  }
+
+  cancelEdit()
+  {
+    this.resetForm();
+  }
+
+
+  toggleEdit()
+  {
+    this.displayLoader(); 
+    this.isViewMode = !this.isViewMode;
+    this.isViewMode ? this.signupForm.disable() : this.signupForm.enable();
+
+  }
+  showLoader : boolean = false;
+
+  timer : any;
+
+  displayLoader(){
+    this.showLoader = true;
+    this.timer = setTimeout(() => {
+      this.showLoader = false;
+    }, 1000);
+
+  }
+
   displayPass(){
     this.showPass = !this.showPass;
   }
-  resetForm() {
-    this.myForm.reset({
-      fname: '',
-      lname: '',
-      pass: '',
-      phone: '',
-      email: ''
-    });
+
+
+  submitSignupForm(form : FormGroup){
+    console.log(form.value);
   }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.timer);
+  }
+
  
 }
 
