e74101801bef87278add99ae049b776ba2e2ffe6
Habiba <habiba.elshaer004@gmail.com>
2024-12-12 19:04:17 +0200

    movie list page done

--- src/app/pages/customer/list-movie-page/list-movie-page.component.ts
+++ src/app/pages/customer/list-movie-page/list-movie-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, FormGroup, FormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-list-movie-page',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListMoviePageComponent implements OnInit {
 
+  selectedValue: string = "";
+
   constructor() { }
+
+  searchForm : FormGroup = new FormGroup({
+    searchHeader : new FormControl(null),
+    searchFilter : new FormControl(null),
+  })
+
+  cinemas: any[] = [
+    {id: "c1", value:"Cinema1"},
+    {id: "c2", value:"Cinema2"},
+    {id: "c3", value:"Cinema3"},
+    {id: "c4", value:"Cinema4"},
+    {id: "c5", value:"Cinema5"},
+  ];
 
   ngOnInit() {
   }
 
+  submitSearchForm(form : FormGroup){
+    console.log(form.value);
+  }
 }
