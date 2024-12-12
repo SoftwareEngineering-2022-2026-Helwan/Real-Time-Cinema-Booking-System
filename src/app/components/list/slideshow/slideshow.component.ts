29e01c2c64a6affabc9069eefef06038cec0fba0
Tiger <mai.mekky123470@gmail.com>
2024-12-12 22:47:46 +0200

    admin-home done

--- src/app/components/list/slideshow/slideshow.component.ts
+++ src/app/components/list/slideshow/slideshow.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slideshow.component.css']
 })
 export class SlideshowComponent implements OnInit {
+  movies = [
+    {image: '../../../../assets/imgs/maxresdefault.jpg'},
+    {image: '../../../../assets/imgs/maxresdefault (1).jpg'},
+    {image: '../../../../assets/imgs/BeautyAndTheBeast-LiveAction-HollywoodEnglishMoviePoster_23bd9882-b84d-4070-a69b-e72d8a87633e.jpg'},
+    {image: '../../../../assets/imgs/x720.jpg'}
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
