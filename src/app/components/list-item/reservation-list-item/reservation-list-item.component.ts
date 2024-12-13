99dc85f4a4ff6854248f5c51659bc4c351517d8d
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2024-12-13 02:44:23 +0200

    reservation list added to header

--- src/app/components/list-item/reservation-list-item/reservation-list-item.component.ts
+++ src/app/components/list-item/reservation-list-item/reservation-list-item.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit , Input} from '@angular/core';
 export class ReservationListItemComponent implements OnInit {
 
   constructor() { }
-  @Input() reservations: any[] = [];
+  @Input() reservation!: any;
 
 
   cancelReservation(reservationNumber : number) {
